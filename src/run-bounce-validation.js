4feeab46a3519966deda8f0b629e6979818671dc
Declan Naughton <declan.naughton2@mail.dcu.ie>
2021-11-18 15:20:04 +0000

    reconciling!

--- src/run-bounce-validation.js
+++ src/run-bounce-validation.js
@@ -2,8 +2,8 @@ var m = require('./bounce-validation');
 
 var out = [...Array(100).keys()].map((t_in) => ({
   t_in,
-  impact_x: Math.round(m.impact_x({ t_in: t_in })),
-  impact_y: Math.round(m.impact_y({ t_in: t_in })),
+  impact_x: m.impact_x({ t_in: t_in }),
+  impact_y: m.impact_y({ t_in: t_in }),
   impact_dy: Math.round(m.impact_dy({ t_in: t_in })),
 }));
 
